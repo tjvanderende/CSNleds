6ce2d44ef596de9183e151b3cb1f45d983ef08ff
tjvanderende <tjvanderende@hotmail.com>
2016-11-03 13:36:29 +0100

    new 0.1

--- python/test.py
+++ python/test.py
@@ -1,14 +1,18 @@
 from socketIO_client import SocketIO
 import logging
 logging.basicConfig(level=logging.DEBUG)
+socketIO = SocketIO('http://boiling-peak-90026.herokuapp.com', port=None)
 
-def on_bbb_response(*args):
-    print('on_bbb_response', args)
+def on_led_value(*args):
+    '''
+    Wordt aangeroepen op basis van led_switch
+    :param args: Data object { led: id van ledje, status: 1 = aan, 0 = uit;
+    :return: None
+    '''
+    data = args[0]
+    value = float(data['value'])
+    socketIO.emit('led_value', data)
 
-def on_aaa_response(*args):
-    print('on_aaa_response', args)
 
-with SocketIO('http://boiling-peak-90026.herokuapp.com', port=None) as socketIO:
-    socketIO.emit('lighton', 'new-tx')
-    socketIO.on('led', on_aaa_response)
-    socketIO.wait()
+socketIO.on('led_value', on_led_value)
+socketIO.wait()
