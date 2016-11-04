548eb74ec7ac01e2616db10c8ec0c85048a6d2cd
tjvanderende <tjvanderende@hotmail.com>
2016-11-04 08:58:43 +0100

    Documentatie bijgwerkt / toegevoegd

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ var allowedOrigins = "*:*";
 var io = require('socket.io').listen(server, {origins:allowedOrigins});
 var fs = require('fs');
 
-server.listen(process.env.PORT || 5000);
+server.listen(process.env.PORT || 5000); // luister op poort 5000 tenzij anders aangegeven via de opties in Heroku.
 
 /*function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
@@ -22,14 +22,21 @@ server.listen(process.env.PORT || 5000);
   });
 }*/
 
+/**
+ * Connectie gemaakt
+ */
 io.on('connection', function (socket) {
-  console.log("Client connected");
-  console.log(Object.keys(io.engine.clients))
-  socket.on('disconnect', function(){
+  console.log("Client connected"); 
+  console.log(Object.keys(io.engine.clients)); // print connected clients (voor debug).
+  socket.on('disconnect', function(){ // kijk of een connectie is verbroken
     console.log("Client disconnected");
   });
+  /**
+   * Ontvang data
+   */
   socket.on('led_value', function (data) {
     console.log(data);
+    // stuur dit aan alle clients door BEHALVE de client waar het pakketje led_value weg komt!
     socket.broadcast.emit('led_value', data); // { led: id van ledje, status: 1 == aan, 0 == uit }
   });
 
